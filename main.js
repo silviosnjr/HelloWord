86b0656ff665cc9e2027823936a02d61d211238d
silviosnjr <silviosnjr@gmail.com>
2023-11-30 08:09:29 -0300

    exemplificando alguns conceitos de lógica

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let nome = prompt("Qual o seu nome ?");
-let elementoSaudacoes = document.querySelector("#saudações");
-elementoSaudacoes.textContent = "Bem-vindo, " + nome;
+function perguntaNome(){
+    let resposta = prompt("Qual o seu nome ?");    
+    return resposta;
+}
+
+let nome = perguntaNome();
+
+while (nome === ""){
+    nome = perguntaNome();
+}
+
+if (nome !== ""){
+    const elementoSaudacoes = document.querySelector("#saudações");
+    elementoSaudacoes.textContent = "Bem-vindo, " + nome;
+    alert(nome + ", seja bem vindo a minha primeira página web");        
+}else{
+    alert("Seja bem vindo a minha primeira página web");
+}
